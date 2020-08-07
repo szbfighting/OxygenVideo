2e329fb792987fe16d8d9042d1f55b9d74c73ec1
martin <1156509299@qq.com>
2020-08-07 11:12:14 +0800

    修复vp滑动bnv不改变状态的问题

--- app/src/main/java/com.oxygenvideo/ui/MainActivity.kt
+++ app/src/main/java/com.oxygenvideo/ui/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.oxygenvideo.ui
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
 import com.oxygenvideo.ui.adapter.MainViewPagerAdapter
 import com.example.tablayout.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,10 +22,28 @@ class MainActivity : AppCompatActivity() {
             adapter = mainAdapter
             //set缓存页数
             offscreenPageLimit = 1
+            addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+
+                override fun onPageScrollStateChanged(state: Int) {
+
+                }
+
+                override fun onPageScrolled(
+                    position: Int,
+                    positionOffset: Float,
+                    positionOffsetPixels: Int
+                ) {
+                }
+
+                override fun onPageSelected(position: Int) {
+                    //当vp页面改变后也会改变bnv的item选中状态
+                    bottomNavigationView.menu.getItem(position).isChecked = true
+                }
+            })
         }
 
-        //没有类似setUpWithViewPager(vp)的方法, 而是以这种选择item后使vp做出换页的方式
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            //当bnv选中后会改变vp的显示页面
             when (item.itemId) {
                 R.id.nav_home -> {
                     viewPager.currentItem = 0
@@ -36,5 +57,21 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+        bottomNavigationView.setOnNavigationItemSelectedListener(object:BottomNavigationView.OnNavigationItemSelectedListener{
+            override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+                when (p0.itemId) {
+                    R.id.nav_home -> {
+                        viewPager.currentItem = 0
+                    }
+                    R.id.nav_message -> {
+                        viewPager.currentItem = 1
+                    }
+                    R.id.nav_person -> {
+                        viewPager.currentItem = 2
+                    }
+                }
+                return false
+            }
+        })
     }
 }
